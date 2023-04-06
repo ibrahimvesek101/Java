adf0305b31a0abadca6cdc1de9ae64a0c0fbca23
ibrahimvesek101 <ibrahimvesek101@gmail.com>
2023-04-06 16:27:37 +0300

    6nisan

--- src/j99_Lambda/lambdaTutorial/C01_LambdaExpression.java
+++ src/j99_Lambda/lambdaTutorial/C01_LambdaExpression.java
@@ -55,8 +55,6 @@ public class C01_LambdaExpression {
         System.out.println("\n   task03   ");
         printElfunctional2(sayiList);
 
-
-
     }//main sonu
     //task 01->  "Structured Programming"--> AMELE Programming kullanara list elemanlarını aynı satırda aralarında bosluk olacak şekilde print ediniz.
 
