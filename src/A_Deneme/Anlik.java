4b12fffa0bd5a8be691611de7f2430d2d32b77c4
ibrahimvesek101 <ibrahimvesek101@gmail.com>
2023-07-10 12:08:05 +0300

    10_tem

--- src/A_Deneme/Anlik.java
+++ src/A_Deneme/Anlik.java
@@ -4,10 +4,6 @@ import java.util.*;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-
-import static java.util.Arrays.asList;
-import static java.util.Arrays.sort;
-
 public class Anlik {
     static Scanner input = new Scanner(System.in);
 
