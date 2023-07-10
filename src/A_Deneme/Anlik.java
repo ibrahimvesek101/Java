f7c118b8d7d271f4bbaaa0149e5c1a3fe27e249e
ibrahimvesek101 <ibrahimvesek101@gmail.com>
2023-07-10 12:09:09 +0300

    10_tem

--- src/A_Deneme/Anlik.java
+++ src/A_Deneme/Anlik.java
@@ -4,10 +4,8 @@ import java.util.*;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-
 public class Anlik {
     static Scanner input = new Scanner(System.in);
-
     public static void main(String[] args) {
 
         ArrayList<Integer> arrList = new ArrayList<>(Arrays.asList(12, 12, 21, 21, 34));
